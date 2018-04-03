18c5e33f73b401772766eda46e687d2aaa9f288e
Byrne, Jessica <Jessica.Byrne@cdk.com>
2018-04-03 15:50:50 -0700

    added mdc primary and accent shades

--- src/theme/DefaultTheme.js
+++ src/theme/DefaultTheme.js
@@ -1,27 +1,39 @@
 const core = {
-  white: '#fff',
-  gray: '#565a5c',
-  grayLight: '#82888a',
-  grayLighter: '#cacccd',
-  grayLightest: '#f2f2f2',
+  white: "#fff",
+  gray: "#565a5c",
+  grayLight: "#82888a",
+  grayLighter: "#cacccd",
+  grayLightest: "#f2f2f2",
 
-  borderMedium: '#c4c4c4',
-  border: '#dbdbdb',
-  borderLight: '#e4e7e7',
-  borderLighter: '#eceeee',
-  borderBright: '#f4f5f5',
+  borderMedium: "#c4c4c4",
+  border: "#dbdbdb",
+  borderLight: "#e4e7e7",
+  borderLighter: "#eceeee",
+  borderBright: "#f4f5f5",
 
-  primary: '#00a699',
-  primaryShade_1: '#33dacd',
-  primaryShade_2: '#66e2da',
-  primaryShade_3: '#80e8e0',
-  primaryShade_4: '#b2f1ec',
-  primary_dark: '#008489',
+  primary: "#00a699",
+  primaryShade_1: "#33dacd",
+  primaryShade_2: "#66e2da",
+  primaryShade_3: "#80e8e0",
+  primaryShade_4: "#b2f1ec",
+  primary_dark: "#008489",
 
-  secondary: '#007a87',
+  secondary: "#007a87",
 
-  yellow: '#ffe8bc',
-  yellow_dark: '#ffce71',
+  yellow: "#ffe8bc",
+  yellow_dark: "#ffce71",
+
+  mdcThemePrimary: "#004e9a",
+  mdcThemePrimaryShade_1: "#2669A9",
+  mdcThemePrimaryShade_2: "#4D83B8",
+  mdcThemePrimaryShade_3: "#80A7CD",
+  mdcThemePrimaryShade_4: "#b2f1ec",
+  mdcThemeAccent: "#0E79CC",
+  mdcThemeBackground: "#fff",
+  mdcTypographyFont_1: "Roboto",
+  mdcTypographyFont_2: "Arial",
+  mdcTypographyFont_3: "sans-serif",
+  cdk_dark: "#000"
 };
 
 export default {
@@ -32,29 +44,28 @@ export default {
         border: 0,
         borderTop: 0,
         borderRight: 0,
-        borderBottom: '2px solid transparent',
+        borderBottom: "2px solid transparent",
         borderLeft: 0,
         outlineFocused: 0,
         borderFocused: 0,
         borderTopFocused: 0,
         borderLeftFocused: 0,
         borderBottomFocused: `2px solid ${core.primary_dark}`,
-        borderRightFocused: 0,
-      },
+        borderRightFocused: 0
+      }
     },
 
     color: {
       core,
-
       disabled: core.grayLightest,
 
       background: core.white,
-      backgroundDark: '#f2f2f2',
+      backgroundDark: "#f2f2f2",
       backgroundFocused: core.white,
       text: core.gray,
       textDisabled: core.border,
-      textFocused: '#007a87',
-      placeholderText: '#757575',
+      textFocused: "#007a87",
+      placeholderText: "#757575",
 
       outside: {
         backgroundColor: core.white,
@@ -62,7 +73,7 @@ export default {
         backgroundColor_hover: core.white,
         color: core.gray,
         color_active: core.gray,
-        color_hover: core.gray,
+        color_hover: core.gray
       },
 
       highlighted: {
@@ -71,7 +82,7 @@ export default {
         backgroundColor_hover: core.yellow_dark,
         color: core.gray,
         color_active: core.gray,
-        color_hover: core.gray,
+        color_hover: core.gray
       },
 
       minimumNights: {
@@ -81,43 +92,43 @@ export default {
         borderColor: core.borderLighter,
         color: core.grayLighter,
         color_active: core.grayLighter,
-        color_hover: core.grayLighter,
+        color_hover: core.grayLighter
       },
 
       hoveredSpan: {
-        backgroundColor: core.primaryShade_4,
-        backgroundColor_active: core.primaryShade_3,
-        backgroundColor_hover: core.primaryShade_4,
-        borderColor: core.primaryShade_3,
-        borderColor_active: core.primaryShade_3,
-        borderColor_hover: core.primaryShade_3,
+        backgroundColor: core.mdcThemePrimaryShade_4,
+        backgroundColor_active: core.mdcThemePrimaryShade_3,
+        backgroundColor_hover: core.mdcThemePrimaryShade_4,
+        borderColor: core.mdcThemePrimaryShade_3,
+        borderColor_active: core.mdcThemePrimaryShade_3,
+        borderColor_hover: core.mdcThemePrimaryShade_3,
         color: core.secondary,
         color_active: core.secondary,
-        color_hover: core.secondary,
+        color_hover: core.secondary
       },
 
       selectedSpan: {
-        backgroundColor: core.primaryShade_2,
-        backgroundColor_active: core.primaryShade_1,
-        backgroundColor_hover: core.primaryShade_1,
-        borderColor: core.primaryShade_1,
-        borderColor_active: core.primary,
-        borderColor_hover: core.primary,
+        backgroundColor: core.mdcThemePrimaryShade_2,
+        backgroundColor_active: core.mdcThemePrimaryShade_1,
+        backgroundColor_hover: core.mdcThemePrimaryShade_1,
+        borderColor: core.mdcThemePrimaryShade_1,
+        borderColor_active: core.mdcThemePrimary,
+        borderColor_hover: core.mdcThemePrimary,
         color: core.white,
         color_active: core.white,
-        color_hover: core.white,
+        color_hover: core.white
       },
 
       selected: {
-        backgroundColor: core.primary,
-        backgroundColor_active: core.primary,
-        backgroundColor_hover: core.primary,
-        borderColor: core.primary,
-        borderColor_active: core.primary,
-        borderColor_hover: core.primary,
+        backgroundColor: core.mdcThemePrimary,
+        backgroundColor_active: core.mdcThemePrimary,
+        backgroundColor_hover: core.mdcThemePrimary,
+        borderColor: core.mdcThemePrimary,
+        borderColor_active: core.mdcThemePrimary,
+        borderColor_hover: core.mdcThemePrimary,
         color: core.white,
         color_active: core.white,
-        color_hover: core.white,
+        color_hover: core.white
       },
 
       blocked_calendar: {
@@ -129,7 +140,7 @@ export default {
         borderColor_hover: core.grayLighter,
         color: core.grayLight,
         color_active: core.grayLight,
-        color_hover: core.grayLight,
+        color_hover: core.grayLight
       },
 
       blocked_out_of_range: {
@@ -141,8 +152,8 @@ export default {
         borderColor_hover: core.borderLight,
         color: core.grayLighter,
         color_active: core.grayLighter,
-        color_hover: core.grayLighter,
-      },
+        color_hover: core.grayLighter
+      }
     },
 
     spacing: {
@@ -160,14 +171,14 @@ export default {
       displayTextPaddingBottom_small: 6,
       displayTextPaddingHorizontal_small: undefined,
       displayTextPaddingLeft_small: 8,
-      displayTextPaddingRight_small: 8,
+      displayTextPaddingRight_small: 8
     },
 
     sizing: {
       inputWidth: 130,
       inputWidth_small: 90,
       arrowWidth: 24,
-      arrowWidth_small: 19,
+      arrowWidth_small: 19
     },
 
     font: {
@@ -175,11 +186,11 @@ export default {
       captionSize: 18,
       input: {
         size: 18,
-        lineHeight: '24px',
+        lineHeight: "24px",
         size_small: 14,
-        lineHeight_small: '18px',
-        styleDisabled: 'italic',
-      },
-    },
-  },
+        lineHeight_small: "18px",
+        styleDisabled: "italic"
+      }
+    }
+  }
 };
